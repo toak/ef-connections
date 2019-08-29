9cc6762d58bd763bcd63929df28aca2726f8be7c
toak <info@toak.de>
2019-08-29 13:52:50 +0200

    Getting API Info

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="form">Test</div>
+  <div class="form">
+    <h1>Bitte ausfüllen</h1>Ich gehöre zu:
+    <select v-model="location_f.name" v-bind:value="location_f.id">
+      <option selected="1" value>Bitte wählen</option>
+      <option v-for="location in locations" :key="location.id">{{ location.name }}</option>
+    </select>
+    <br>Am öftesten arbeite ich mit:
+    <select v-model="location_t.name" v-bind:value="location_t.id">
+      <option selected="1" value>Bitte wählen</option>
+      <option v-for="location in locations" :key="location.id">{{ location.name }}</option>
+    </select>
+    <br>Dabei ist mir folgendes am Wichtigsten:
+    <select v-model="value.name" v-bind:value="value.id">
+      <option selected="1" value>Bitte wählen</option>
+      <option v-for="value in values" :key="value.id">{{ value.name }}</option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Calculator",
+  name: "Form",
   data() {
     return {
       locations: [],
-      values: []
+      location_f: [],
+      location_t: [],
+      values: [],
+      value: []
     };
   },
   created() {
@@ -16,6 +35,11 @@ export default {
       .get("https://lernen.toak.de/api/locations")
       .then(function(response) {
         this.locations = response.data;
+      });
+    this.$http
+      .get("https://lernen.toak.de/api/values")
+      .then(function(response) {
+        this.values = response.data;
       });
   }
 };
