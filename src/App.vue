97f4e234e2725070a3b9a3257ee29e3a87dc082f
toak <info@toak.de>
2019-09-18 15:12:34 +0200

    Changed wording

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <center>
       <img src="eurofunk-logo.svg">
     </center>
-    <h1>Deine Verbindung zu eurofunk Kollegen (m/w/d)</h1>
+    <h1>Deine Verbindung zu eurofunk Kollegen*innen</h1>
     <p>Personen an unterschiedlichen Standorten arbeiten für eurofunk und deren Services, Lösungen und Produkte. Jeder von uns ist geprägt von verschiedenen Werten, Kulturen, Gewohnheiten und Kommunikationswegen.</p>
     <p>
-      Mit welchen Kollegen (m/w/d) arbeitest du außerhalb deines Bereichs / Standorts zusammen? (der eigene Standort/Bereich ist nicht wählbar!)
+      Mit welchen Kollegen*innen arbeitest du außerhalb deines Bereichs / Standorts zusammen? (der eigene Standort/Bereich ist nicht wählbar!)
       Was ist dir dabei am wichtigsten? Das wollen wir visualisieren - dazu fülle bitte diese Formular aus - gerne auch mehrmals!
     </p>
     <h1>Your connection to eurofunk colleagues</h1>
